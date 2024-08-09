7c2bcd49e20a8c7de32acab6464f1349f59d2ec4
SOVANNARO <sovannaro066@gmail.com>
2024-08-09 23:42:48 +0700

    fix: deploy not found page

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <BrowserRouter basename="/react-query-game/">
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
